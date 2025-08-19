6714fb9725d93fadbbd93bd78212692c562847b9
SantiDrelewicz <santidrelewicz2000@gmail.com>
2025-08-19 01:21:14 -0300

    Refactor TexRec initialization to download model if not cached and load state from local path

--- texrec/reconstructor/texrec.py
+++ texrec/reconstructor/texrec.py
@@ -1,4 +1,7 @@
 from .model import TexRecModel
+from pathlib import Path
+import requests
+import torch
 
 
 class TexRec:
@@ -7,10 +10,30 @@ class TexRec:
     for text recognition tasks.
     """
 
-    def __init__(self):
-        # Additional initialization can be added here if needed
-        self.__model = TexRecModel()
-        self.__model.load_model("_texrec_model.pt")
+    def __init__(self, filename="texrec_model.pt", version="v0.1.0"):
+        # URL del release en GitHub
+        url = f"https://github.com/SantiDrelewicz/pitext/releases/download/{version}/{filename}"
+        
+        # Cache local (~/.cache/pitext/texrec_model.pt)
+        cache_path = Path.home() / ".cache" / "pitext" / filename
+        cache_path.parent.mkdir(parents=True, exist_ok=True)
+
+         # Descargar si no está en caché
+        if not cache_path.exists():
+            print(f"Descargando modelo desde {url}...")
+            r = requests.get(url)
+            r.raise_for_status()
+            with open(cache_path, "wb") as f:
+                f.write(r.content)
+            print("Descarga completada ✅")
+
+        # Cargar el modelo
+        state = torch.load(cache_path, map_location="cpu", )
+        model = TexRecModel()  # tu clase definida en pitext
+        model.load_state_dict(state)
+        model.eval()
+        self.__model = model
+
 
     def __call__(self, text: str) -> str:
         """
